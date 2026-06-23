f24efa959bb3df2840822f55f8b408944bba17f3
Matthew Carreon <mcda@duck.com>
2026-06-23 05:18:00 -0700

    Added base code for splitting the check including only using num pad

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberofPeople = 2
+    @State private var tipPercentage = 15
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Text("Hello World!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) //Get the user's local currency and get the currency code otherwise default to USD
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+        }
     }
 }
 
