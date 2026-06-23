25450f24b1593484cd84e0fb81b19d47ee8ec272
Matthew Carreon <mcda@duck.com>
2026-06-23 05:55:05 -0700

    Created a  Focus state to allow the user to get out when using the num pad

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 15
-    
+    @FocusState private var amountIsFocused: Bool
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
@@ -31,6 +31,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) //Get the user's local currency and get the currency code otherwise default to USD
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -55,6 +56,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if (amountIsFocused) {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
