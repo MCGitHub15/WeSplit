e25a01bdcee243991918043636a96f6eba1ee70c
Matthew Carreon <mcda@duck.com>
2026-06-23 02:29:29 -0700

    Removed NavigationStack and Added Click able button

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,28 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount: Int = 0
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello World!")
-                }
-                
-                Section {
-                    Text("Hello World!")
-                    Text("Hello World!")
-                    Text("Hello World!")
-                }
-                Section {
-                    Text("Hello World!")
-                    Text("Hello World!")
-                    Text("Hello World!")
-                    Text("Hello World!")
-                    Text("Hello World!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
 }
